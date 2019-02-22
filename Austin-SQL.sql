d45f869b046c2f1ffd1228fc27025dcc71494026
jisashaji64 <47326583+jisashaji64@users.noreply.github.com>
2019-02-23 00:57:27 +0530

    Minor Corrections on queries

--- Austin-SQL.sql
+++ Austin-SQL.sql
@@ -1,11 +1,23 @@
 /* 
+----------------------------------------------------------------------------------------------------------------
+                           [Querying Public Dataset Using SQL on Google Bigquery] 
+----------------------------------------------------------------------------------------------------------------
 PUBLIC DATASET URL   : https://console.cloud.google.com/marketplace/details/city-of-austin/austin-crime
-DATASET DESCRIPTION  :
-This dataset includes Part 1 crimes (as defined by Uniform Crime Reporting Statistics ) for 2014 and 2015. Data is provided by the Austin Police Department and may differ from official APD crime data due to the variety of reporting and collection methods used.
---------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------
+_________________________________________________________________________________________________________________
+                      |This dataset includes Part 1 crimes (as defined by Uniform Crime Reporting Statistics )
+DATASET DESCRIPTION   |Data is provided by the Austin Police Department and may differ from official APD                          
+		      |crime data due to the variety of reporting and collection methods used.
+-----------------------------------------------------------------------------------------------------------------
+|-------------------------------------------------------- | 
+| AUTHOR           : JISA SHAJI                           |
+| DATE OF CREATION : 22-02-2019				  |
+| EMAIL            : jisa.shaji@eco.christuniversity.in   |
+|---------------------------------------------------------|
+
+					    ***[SQL QUERIES]***
+
 1.This query would display first 1000 rows of database with all of its attributes.
---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------
 */
 
 SELECT * 
@@ -50,8 +62,9 @@ SELECT primary_type,count(primary_type)
 FROM `bigquery-public-data.austin_crime.crime`
 GROUP BY primary_type
 \*
+
 -----------------------------------------------------------------------------------------------------
-7. This joins two tables from two diffrent database[1.AUSTIN CRIME DATABASE 2.CHICAGO CRIME DATABASE]
+7.[JOIN] This joins two tables from two diffrent database[1.AUSTIN CRIME DATABASE 2.CHICAGO CRIME DATABASE]
    and display the address of crime scene where the crime incident years are matching in the two tables
 -----------------------------------------------------------------------------------------------------
 */
@@ -60,12 +73,13 @@ INNER JOIN `bigquery-public-data.chicago_crime.crime` chicago
 ON austin.year = chicago.year
 \*
 -----------------------------------------------------------------------------------------------------
-8. Returns the latitude and longitude of Crime Scene from Chicago crime database and Austin Crime database
+8.[JOIN] Returns the latitude and longitude of the Crime Scene from Chicago crime database and Austin Crime database
+   where the year of crime is same
 -----------------------------------------------------------------------------------------------------
 */
-SELECT austin.latitude,austin.longitude,chicago.x_coordinate,chicago.y_coordinate FROM `bigquery-public-data.austin_crime.crime` austin
-FROM `bigquery-public-data.chicago_crime.crime` chicago
-ON austin.year = chicago.year
+SELECT austin.latitude,austin.longitude,chicago.x_coordinate,chicago.y_coordinate
+FROM `bigquery-public-data.austin_crime.crime` austin
+INNER JOIN `bigquery-public-data.chicago_crime.crime` chicago ON chicago.year=austin.year
 \*
 -----------------------------------------------------------------------------------------------------
 9. Returns unique_key and description based on the descending order of clearence_date
@@ -83,3 +97,10 @@ ORDER BY clearance_date DESC
 SELECT * FROM
 `bigquery-public-data.austin_crime.crime` 
 WHERE primary_type LIKE 'R%e'
+/*
+ ________________________________________________________________________________________________________________________
+|NOTE : Due to the limitations of types of datas available inside the choosen dataset, Joins have been made on unrelated |
+|       data attributes                                                                                                  |
+|________________________________________________________________________________________________________________________|
+
+*/
